cfeeb5b2192fe7c777c782276ac0dc6db2a74a0e
Ziqi Wang <qiqiustc@gmail.com>
2019-04-14 13:46:24 +0200

    add student version1.0

--- blender/MeshPatternBlender.py
+++ blender/MeshPatternBlender.py
@@ -6,27 +6,25 @@ from compas.datastructures import Mesh
 from compas.plotters import MeshPlotter
 from compas_blender.utilities import xdraw_mesh, xdraw_cylinders
 from compas.geometry import *
+from tessagon.types.hex_tessagon import HexTessagon
+from tessagon.adaptors.list_adaptor import ListAdaptor
 
+# get exact directory of current blender file
 file_dir = os.path.dirname(os.path.realpath(__file__))
 dir_path = os.path.dirname(file_dir) 
 sys.path.append(file_dir)
 sys.path.append(file_dir + "/../lib")
 sys.path.append(dir_path + "/../lib")
 
-import MeshPattern
 from MeshPattern import *
 
-from tessagon.types.hex_tessagon import HexTessagon
-from tessagon.adaptors.list_adaptor import ListAdaptor
-
+# Draw Mesh with vertices and faces
 def drawMeshVF(v, f):
 	xdraw_mesh(v, None, f)
 
-def drawMeshVEF(v, e, f):
-	xdraw_mesh(v, e, f)
-
+# Create a 2D Hexagon pattern in [0, 1]x[0, 1]
+# return a list of [vertices, faces]
 def create2DPattern():
-	#Create 2D pattern in [-1, 1]x[-1,1]
 	options = {
     	'function': lambda u, v: [u, v, 0],
     	'u_range': [0, 1.0],
@@ -40,47 +38,15 @@ def create2DPattern():
 	tessagon = HexTessagon(**options)
 	vertices = tessagon.create_mesh()['vert_list']
 	faces = tessagon.create_mesh()['face_list']
-	mesh = iglMesh()
-	mesh.loadMesh(vertices, faces)
-	return mesh
-
+	return [vertices, faces]
 
 if __name__ == "__main__":
-	#Use compas to read obj file 
-	surfaceOBJ = Mesh.from_obj(dir_path + '/../data/minimal_surface.obj')
-	#surfaceOBJ = Mesh.from_obj(dir_path + '/data/minimal_surface.obj')
-	
-	vertices, faces = surfaceOBJ.to_vertices_and_faces()
-	for key, attr in surfaceOBJ.vertices(True):
-		attr['x'] = vertices[key][0]
-		attr['y'] = -vertices[key][2]
-		attr['z'] = vertices[key][1]
+    # you can change here to import your own file, like :
+    # surfaceOBJ = Mesh.from_obj(dir_path + '/../data/igloo.obj')
+    surfaceOBJ = Mesh.from_obj(dir_path + '/../data/minimal_surface.obj')
 
-	vertices, faces = surfaceOBJ.to_vertices_and_faces()
+    #getting the vertices and faces list from compas mesh
+    vertices, faces = surfaceOBJ.to_vertices_and_faces()
 
-	#Create a C++ iglMesh class and enter input OBJ mesh
-	mesh = iglMesh()
-	mesh.loadMesh(vertices, faces)
-	mesh.paramertization_lscm()
-
-	#Process mesh
-	pattern2D = create2DPattern()
-	pattern2D.paramertization_simple()
-	mesh.mapMesh3D_AABB(pattern2D)
-
-    #ShapeOp
-	solver = ShapeOpt()
-	solver.loadMesh(pattern2D)
-	solver.addPlanarConstraint(1.0)
-	solver.addBoundaryConstraint(0.5)
-	solver.addRegularConstraint(1.0)
-	solver.runShapeOp(10)
-	pattern3D = solver.getMesh()
-
-	#Return proccessd mesh and get its vertices and faces
-
-	
-	wire = pattern3D.saveWireFrame(0.01, 10)
-	vertices = wire.getVertices()
-	faces = wire.getFaces()
-	drawMeshVF(vertices, faces)
+    #draw the mesh in the blender scene
+    drawMeshVF(vertices, faces)
--- src/MeshPattern.cpp
+++ src/MeshPattern.cpp
@@ -9,20 +9,14 @@
 namespace py = pybind11;
 using namespace pybind11::literals;
 
-
 PYBIND11_MODULE(MeshPattern, m)
 {
     py::class_<iglMesh>(m, "iglMesh")
             .def(py::init())
             .def("loadMesh", &iglMesh::loadMesh)
-            .def("paramertization_simple", &iglMesh::paramertization_simple)
-            .def("paramertization_lscm", &iglMesh::paramertization_lscm)
-            .def("mapMesh3D_simple", &iglMesh::mapMesh3D_simple)
-            .def("mapMesh3D_AABB", &iglMesh::mapMesh3D_AABB)
-            .def("saveWireFrame", &iglMesh::saveWireFrame)
             .def("getUVs", &iglMesh::getUVs)
             .def("getVertices",  &iglMesh::getVertices)
-            .def("getFaces",  &iglMesh::getFaces);
+            .def("getFaces",  &iglMesh::getFaces)
 
 
     py::class_<ShapeOpt>(m, "ShapeOpt")
